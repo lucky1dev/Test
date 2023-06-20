625c066f4991cfa0487eb8dc6476081f5695a7c3
Luc Pr <135625805+lucky1dev@users.noreply.github.com>
2023-06-20 18:47:55 +0200

    Changes

---
+++ docs/precache-manifest.91ef31177f44e163a89460e3f50e4540.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9d2d1accb231dde0c5c8d1f9cc402a",
+    "url": "/Test/index.html"
+  },
+  {
+    "revision": "949004dff5e405270098",
+    "url": "/Test/static/css/main.0c1080f5.chunk.css"
+  },
+  {
+    "revision": "949004dff5e405270098",
+    "url": "/Test/static/js/main.949004df.chunk.js"
+  },
+  {
+    "revision": "5ad6983d07ee2c528d46",
+    "url": "/Test/static/js/runtime~main.5ad6983d.js"
+  },
+  {
+    "revision": "1d2904fc80f3d16cba65",
+    "url": "/Test/static/js/vendors~main.1d2904fc.chunk.js"
+  }
+]);
